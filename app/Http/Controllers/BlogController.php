a2e9876d66ec960cb0fdd4fb28867e28de6d1ec6
Amirul Faiz <faizshamsul98@gmail.com>
2022-11-26 14:00:49 +0800

    Add create and store function

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -12,4 +12,34 @@ class BlogController extends Controller
         $blogs = Blog::all();
         return view('blogs.index', compact('blogs'));
     }
+
+    public function create()
+    {
+        return view('blogs.create');
+    }
+
+    public function store(Request $request)
+    {
+        //method 1
+
+        // $blog = new Blog();
+        // $blog->title = $request->title;
+        // $blog->content = $request->content;
+        // $blog->save();
+
+        //method 2
+
+        // Blog::create($request->only('title', 'content'));
+
+        //method 3
+
+        auth()->user()->blogs()->create($request->only('title', 'content'));
+
+        return redirect()->route('blogs.index');
+    }
+
+    public function show(Blog $blog)
+    {
+        return view('blogs.show', compact('blog'));
+    }
 }
--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -5,7 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Index') }}
+                    <a href="{{ route('blogs.create') }}" class="btn btn-primary" style="float:right;">Create</a>
+                </div>
 
                 <div class="card-body">
                   <table class="table">
@@ -14,6 +16,7 @@
                         <th scope="col">ID</th>
                         <th scope="col">Title</th>
                         <th scope="col">Content</th>
+                        <th scope="col">Created By</th>
                         <th scope="col">Created At</th>
                         <th scope="col">Updated At</th>
                         <th scope="col">Action</th>
@@ -25,9 +28,12 @@
                         <td>{{ $blog->id }}</td>
                         <td>{{ $blog->title }}</td>
                         <td>{{ $blog->content }}</td>
+                        <td>{{ $blog->user->name }}</td>
                         <td>{{ $blog->created_at }}</td>
                         <td>{{ $blog->updated_at }}</td>
-                        <td></td>
+                        <td>
+                            <a href="{{ route('blogs.show', $blog) }}" class="btn btn-primary">Show</a>
+                        </td>
                       </tr>
                       @endforeach
                     </tbody>
